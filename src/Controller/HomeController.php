f2663ff8190c45f362d601cd714451a5d68ac753
Gauthier A <gauthier.andre@protonmail.com>
2023-03-09 03:18:39 +0100

    show images

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
-use App\Media\LeMonde;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(#[TaggedIterator('app.media')] private iterable $medias)
+    {
+    }
+
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'medias' => $this->medias
+        ]);
     }
 }
