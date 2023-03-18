645a7d938bb0852f6366c9852f0fe53105f406f7
Gauthier A <gauthier.andre@protonmail.com>
2023-03-19 00:01:41 +0100

    add The Guardian

---
+++ src/Media/GB/TheGuardian.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Media\GB;
+
+use App\Media\MediaInterface;
+
+class TheGuardian implements MediaInterface
+{
+    public function getName(): string
+    {
+        return 'The Guardian';
+    }
+
+    public static function getUrl(): string
+    {
+        return 'https://www.theguardian.com/world/series/ukraine-live';
+    }
+
+    public static function getLocale(): string
+    {
+        return 'en';
+    }
+
+    public static function getCountry(): string
+    {
+        return 'GB';
+    }
+
+    public static function getCustomCss(): string|null
+    {
+        return <<<'CSS'
+            .top-banner-ad-container, .ad-slot-container, iframe {
+                display: none!important
+            }
+            body {
+                margin-top: 0px!important
+            }
+        CSS;
+    }
+
+    public static function getFilename(): string
+    {
+        return 'the_guardian_%s';
+    }
+}
