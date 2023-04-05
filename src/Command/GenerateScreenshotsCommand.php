9ebb5f73cfb2069de4ce5160330fde29d83180dd
Gauthier A <gauthier.andre@protonmail.com>
2023-04-06 00:41:25 +0200

    parallelize screenshot generation

--- src/Command/GenerateScreenshotsCommand.php
+++ src/Command/GenerateScreenshotsCommand.php
@@ -35,14 +35,13 @@ class GenerateScreenshotsCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $filesystem = new Filesystem();
+        $processes = [];
         foreach ($this->medias as $media) {
             $filesystem->mkdir(\sprintf(self::TARGET_PATH, $media->getCountry(), $media->getLocale()));
             $process = new Process([self::PAGERES_BIN, $media->getUrl(), '390x1266', '--scale=2', '--css=' . $media->getCustomCss(), '--crop', '--overwrite', '--filename=' . \sprintf(self::TARGET_PATH, $media->getCountry(), $media->getLocale()) . $media->getFilename()]);
             $process->run();
-
-            if (!$process->isSuccessful()) {
-                throw new ProcessFailedException($process);
-            }
+            $output->writeln(\sprintf('Generating %s screenshot for locale %s', $media->getName(), $media->getLocale()));
+            $processes[] = $process;
 
             foreach ($this->supportedLocales as $locale) {
                 if ($locale === $media->getLocale()) {
@@ -51,12 +50,21 @@ class GenerateScreenshotsCommand extends Command
                 $filesystem->mkdir(\sprintf(self::TARGET_PATH, $media->getCountry(), $locale));
                 $process = new Process([self::PAGERES_BIN, 'https://translate.google.com/translate?sl=' . $media->getLocale() . '&tl=' . $locale . '&u=' . $media->getUrl(), '390x1266', '--scale=2', '--css=' . $media->getCustomCss(), '--crop', '--overwrite', '--filename=' . \sprintf(self::TARGET_PATH, $media->getCountry(), $locale) . $media->getFilename()]);
                 $process->run();
+                $output->writeln(\sprintf('Generating %s screenshot for locale %s', $media->getName(), $locale));
+                $processes[] = $process;
+            }
 
-                if (!$process->isSuccessful()) {
-                    throw new ProcessFailedException($process);
+            while (\count($processes) > 0) {
+                $output->writeln('Checking processes...');
+                foreach ($processes as $index => $process) {
+                    if (!$process->isRunning()) {
+                        unset($processes[$index]);
+                    }
                 }
+                sleep(1);
             }
         }
+
 
         return Command::SUCCESS;
     }
