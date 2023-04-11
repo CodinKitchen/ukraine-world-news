1cda590d10121ac5a6d9d5dc804ed6682ab94bba
Gauthier A <gauthier.andre@protonmail.com>
2023-04-11 12:30:06 +0200

    log country detection

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,10 +4,10 @@ namespace App\Controller;
 
 use App\Media\MediaInterface;
 use GeoIp2\Database\Reader;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Intl\Locales;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -21,12 +21,14 @@ class IndexController extends AbstractController
     ];
 
     #[Route('/', name: 'index')]
-    public function index(Request $request, Reader $reader): Response
+    public function index(Request $request, Reader $reader, LoggerInterface $logger): Response
     {
         try {
             $country = $reader->country($request->getClientIp());
+            $logger->info('Country detected: ' . $country->country->isoCode);
             $locale = self::COUNTRIES_DEFAULT_LOCALE[$country->country->isoCode] ?? 'en';
         } catch (\Exception $e) {
+            $logger->error($e->getMessage());
             $locale = 'en';
         }
 
