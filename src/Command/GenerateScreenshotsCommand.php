0e0d61ba27ceda6b4d9eb069cb723fce1e6af9d4
Gauthier A <gauthier.andre@protonmail.com>
2023-07-31 23:43:50 +0200

    handle process timeout

--- src/Command/GenerateScreenshotsCommand.php
+++ src/Command/GenerateScreenshotsCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
 use Symfony\Component\Process\Process;
 
 #[AsCommand(
@@ -52,6 +53,13 @@ class GenerateScreenshotsCommand extends Command
                         if (!$process->isSuccessful()) {
                             $output->writeln($process->getErrorOutput());
                         }
+                        unset($processes[$index]);
+                    }
+                    try {
+                        $process->checkTimeout();
+                    } catch (ProcessTimedOutException $exception) {
+                        $output->writeln(\sprintf('%s %s', $process->getCommandLine(), $exception->getMessage()));
+                        $process->stop(0);
                         unset($processes[$index]);
                     }
                 }
